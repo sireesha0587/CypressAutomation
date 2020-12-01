f540ef12c0b67a5855f904c85331c30e6635cfd1
Sireesha <sireeshamf@yahoo.com>
2020-11-30 23:04:43 -0600

    Create Liq-LandingPage.spec.js

---
+++ cypress/integration/examples/Liq-LandingPage.spec.js
@@ -0,0 +1,41 @@
+describe('LiquibaseTestSuite1.0', function()
+ {
+    //Test Case 01 - Assert Elements in Landingpage
+    it('Assert Elements in Liquibase LandingPage', function() 
+
+        {
+    // Navigating the liquibase application
+        cy.visit('https://hub-staging.liquibase.com/landing-page')
+    // Asserting for Liquibase title
+        cy.title().should('eq','Liquibase Hub')
+    // Action Sign In
+        cy.contains('Sign In').click()
+    // Asserting for Signin
+        cy.title().should('eq','Signin')
+        //cy.get('.mat-button-wrapper').click() 
+    //Entering credentials 
+        //Username
+        cy.xpath('//*[@id="signInFormUsername"]').last().type('sireeshaliq')
+        //Password
+        cy.xpath('//*[@id="signInFormPassword"]').last().type('Pdp1@siri')
+        cy.xpath('//*[@name="signInSubmitButton"]').last().click()   
+        
+           
+    })
+ 
+// Second Test Case , as some reaon cypress unable to navigate to application,
+// scripted the  error page.
+
+// Test case 02 - Assert elements in the Liquibase error page
+it ('Assert elements in the Liquibase error page', function()
+
+{
+
+    //Asserting the URL of the current page
+    cy.url().should('include', 'amazoncognito.com/error')
+    //Asserting the Liguibase lable(Signin)
+    cy.title().should('eq','Signin')
+    
+})
+    
+})
